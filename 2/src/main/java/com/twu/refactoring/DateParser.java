9dece1ce56766801da1fa291a28e2c1eae535489
MasonTW <mingxin.wang@thoughtworks.com>
2021-08-14 01:16:30 +0800

    [mingxinwang]p2 make verify year and mouth to methods

--- 2/src/main/java/com/twu/refactoring/DateParser.java
+++ 2/src/main/java/com/twu/refactoring/DateParser.java
@@ -29,27 +29,10 @@ public class DateParser {
     public Date parse() {
         int year, month, date, hour, minute;
 
-        try {
-            String yearString = dateAndTimeString.substring(0, 4);
-            year = Integer.parseInt(yearString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Year string is less than 4 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Year is not an integer");
-        }
-        if (year < 2000 || year > 2012)
-            throw new IllegalArgumentException("Year cannot be less than 2000 or more than 2012");
+        year = verifyYear();
 
-        try {
-            String monthString = dateAndTimeString.substring(5, 7);
-            month = Integer.parseInt(monthString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Month string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Month is not an integer");
-        }
-        if (month < 1 || month > 12)
-            throw new IllegalArgumentException("Month cannot be less than 1 or more than 12");
+
+        month = verifyMonth();
 
         try {
             String dateString = dateAndTimeString.substring(8, 10);
@@ -95,5 +78,35 @@ public class DateParser {
         calendar.set(year, month - 1, date, hour, minute, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
+    }
+
+    private int verifyMonth() {
+        int month;
+        try {
+            String monthString = dateAndTimeString.substring(5, 7);
+            month = Integer.parseInt(monthString);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Month string is less than 2 characters");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Month is not an integer");
+        }
+        if (month < 1 || month > 12)
+            throw new IllegalArgumentException("Month cannot be less than 1 or more than 12");
+        return month;
+    }
+
+    private int verifyYear() {
+        int year;
+        try {
+            String yearString = dateAndTimeString.substring(0, 4);
+            year = Integer.parseInt(yearString);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Year string is less than 4 characters");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Year is not an integer");
+        }
+        if (year < 2000 || year > 2012)
+            throw new IllegalArgumentException("Year cannot be less than 2000 or more than 2012");
+        return year;
     }
 }
